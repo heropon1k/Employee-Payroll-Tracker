b4dc9ee373c05a8dc39f679872522a79b562de4b
unknown <brian.zhen101@gmail.com>
2024-06-30 23:33:10 -0400

    commented out redundant code and added comments

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
+//declare variables
 const employeesArray = [];
 
-let input = true;
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
 const collectEmployees = function () {
   // TODO: Get user input to create and return an array of employee objects
-
+  // set input to true ()
+  let input = true;
   while (input !== false) {
+    //Create employee object to be pushed into employees array
     let employeeObject = {
       firstName: window.prompt("What is their first name"),
       lastName: window.prompt("What is their last name"),
       salary: window.prompt("What is their salary"),
     };
+    //Checked whether salary was a number 
     if(isNaN(employeeObject.salary)==true){
       employeeObject.salary = 0;
       console.log("Salary was not a number")
     }
-    console.log(employeeObject)
+    //Checked whether employeeObjects was correct
+    //console.log(employeeObject)
     employeesArray.push(employeeObject)
+    //Checked whether employeesArray was correct at intermediate steps
+    //console.log(employeesArray)
+
     input = window.confirm("Would you like to continue");
+
+    /**Checked whether cancel was clicked
     if (input == false) {
       console.log("clicked cancel");
 
-    }
+    }*/
 
   }
-  console.log(employeesArray)
+  //Checked whether final employeesArray was correct
+  //console.log(employeesArray)
+
+  //Returns the created array for other functions to use
   return(employeesArray);
 
 }
@@ -35,10 +47,16 @@ const collectEmployees = function () {
 // Display the average salary
 const displayAverageSalary = function (employeesArray) {
   // TODO: Calculate and display the average salary
+  //Declared variables
   let average = 0;
   for(i=0; i < employeesArray.length; i++){
+    //Add all salaries, Number function converts salary to a number because it is a string right now
+    //console.log(typeof employeesArray[i].salary)
     average += Number(employeesArray[i].salary);
+    //Checked average
+    //console.log(average)
   }
+  //Divied average by number of employees
   let totalAvg = average/i;
   console.log(`The average employee salary between our ${i} employee(s) is ${totalAvg}`);
 }
@@ -46,8 +64,10 @@ const displayAverageSalary = function (employeesArray) {
 // Select a random employee
 const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
+  //Create random number gen from 0 to lenght of array
   let randInt = Math.floor(Math.random() * employeesArray.length);
-  console.log(randInt);
+  //Checked randInt
+  //console.log(randInt);
   console.log(`Congratulations to ${employeesArray[randInt].firstName} ${employeesArray[randInt].lastName}, our random drawing winner!`);
 }
 
